926acf929703cabde5af6119e18d7f938e583a16
Anthony Labelle-Voyez <antho.voyez@hotmail.com>
2018-01-27 13:08:53 -0500

    Revert "Change World"
    This reverts commit 59b818d029efa0503e3e5dd02f844b1b0e9e3c21.

--- MTL-GameJam/Assets/Scripts/Map/WorldManager.cs
+++ MTL-GameJam/Assets/Scripts/Map/WorldManager.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WorldManager : MonoBehaviour
-{
+public class WorldManager : MonoBehaviour {
     public static WorldManager instance = null;
 
     private const int MAP_SIZE = 10;
 
-    public GameObject LightWorld;
-    public GameObject DarkWorld;
-
-    private GameObject CurrentWorld;
+    public GameObject Tiles;
     public PlayableCharacter Character;
+    Tile[] tiles;
 
     private void Awake()
     {
@@ -21,63 +18,26 @@ public class WorldManager : MonoBehaviour
             instance = this;
         else if (instance != this)
             Destroy(gameObject);
-
-        CurrentWorld = LightWorld;
-        SetActiveTiles(true);
+        tiles = GameObject.FindObjectsOfType<Tile>();
     }
     // Use this for initialization
-    void Start()
-    {
+    void Start () {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ChangeWorld();
-        }
-    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 
     public Tile GetTileAtPosition(Vector2 position)
     {
-        foreach (Tile tile in transform.Find(CurrentWorld.name).GetComponentsInChildren<Tile>())
-        {
-            if (tile.Position == position)
+        foreach(Tile tile in tiles){
+            if(tile.Position == position)
             {
                 return tile;
             }
         }
         return null;
-    }
-
-    public void ChangeWorld()
-    {
-        if (CurrentWorld == LightWorld)
-        {
-            CurrentWorld = DarkWorld;
-            SetActiveTiles(false);
-        }
-        else if (CurrentWorld == DarkWorld)
-        {
-            CurrentWorld = LightWorld;
-            SetActiveTiles(true);
-        }
-    }
-
-    public void SetActiveTiles(bool isLightActive)
-    {
-        for (int i = 0; i < DarkWorld.transform.childCount; i++)
-        {
-            Transform tile = DarkWorld.transform.GetChild(i);
-            tile.gameObject.SetActive(!isLightActive);
-        }
-
-        for (int i = 0; i < LightWorld.transform.childCount; i++)
-        {
-            Transform tile = LightWorld.transform.GetChild(i);
-            tile.gameObject.SetActive(isLightActive);
-        }
     }
 }
--- MTL-GameJam/Assets/Scripts/Units/PlayableCharacter.cs
+++ MTL-GameJam/Assets/Scripts/Units/PlayableCharacter.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayableCharacter : Character {
-    bool Moving = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,58 +13,36 @@ public class PlayableCharacter : Character {
 	// Update is called once per frame
 	void Update () {
         ReactToUserInput();
-        MoveCharacter();
-    }
+	}
 
     public void ReactToUserInput() {
-        if (Moving)
-            return;
-
         if (Input.GetKey(KeyCode.W))
         {
-            SetNextPosition(Position + new Vector2(0, 1));
+            MoveCharacter(Position + new Vector2(0, 1));
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            SetNextPosition(Position + new Vector2(0, -1));
+            MoveCharacter(Position + new Vector2(0, -1));
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            SetNextPosition(Position + new Vector2(-1, 0));
+            MoveCharacter(Position + new Vector2(-1, 0));
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            SetNextPosition(Position + new Vector2(1, 0));
+            MoveCharacter(Position + new Vector2(1, 0));
         }
     }
 
-    public void SetNextPosition(Vector2 nextPosition)
+    public void MoveCharacter(Vector2 NextPosition)
     {
         // VÉRIFIER AVEC LE WORLD SI LA TILE EST TRAVERSABLE
 
-        TileDestination = WorldManager.instance.GetTileAtPosition(nextPosition);
-        if (TileDestination && TileDestination.CanGoThrough)
+        Tile tile = WorldManager.instance.GetTileAtPosition(NextPosition);
+        if (tile && tile.CanGoThrough)
         {
-            Position = nextPosition;
-            //transform.position = tile.transform.position;
-            Moving = true;
+            Position = NextPosition;
+            transform.position = tile.transform.position;
         }
     }
-
-    private void MoveCharacter()
-    {
-        if (!Moving)
-        {
-            return;
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, TileDestination.transform.position, 0.1f);
-        
-        if(Vector3.Distance(transform.position, TileDestination.transform.position) <= 0.1f)
-        {
-            transform.position = TileDestination.transform.position;
-            Moving = false;
-        }
-    }
-
 }
