8e84f60a75ec048f247539475d92a1bd44890cfd
Alexis Parent <a.p_alex@hotmail.com>
2018-01-27 14:21:20 -0500

    Add Door

---
+++ MTL-GameJam/Assets/Scripts/Map/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Tile {
+
+    bool isOpen;
+
+	// Use this for initialization
+	void Start () {
+        isOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenDoor(GameObject thisGameObject)
+    {
+        isOpen = true;
+        CanGoThrough = true;
+    }
+}
