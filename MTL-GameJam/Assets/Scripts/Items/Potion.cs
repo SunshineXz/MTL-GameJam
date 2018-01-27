7bd51d2bc8a34e0f87d9665244dc4ec911fb4e7f
Anthony Labelle-Voyez <AN59970@ens.etsmtl.ca>
2018-01-27 01:30:39 -0500

    Item Start

---
+++ MTL-GameJam/Assets/Scripts/Items/Potion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : Item {
+    public int Health = 50;
+	// Use this for initialization
+	void Start () {
+        Usable = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //LightWorld.GetCharacter().UsePotion(this);
+        }
+    }
+}
