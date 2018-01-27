8d72f87c440fe48e3e1443a077ffd6b2f85d5b28
Anthony Labelle-Voyez <antho.voyez@hotmail.com>
2018-01-26 23:28:41 -0500

    Base Character

---
+++ MTL GameJam/Assets/Scripts/Units/PlayableCharacter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayableCharacter : Character {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ReactToUserInput();
+	}
+
+    public void ReactToUserInput() {
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveCharacter(Position + new Vector2(0, 1) * Time.deltaTime * 32);//TILESIZE
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            MoveCharacter(Position + new Vector2(0, -1) * Time.deltaTime * 32);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            MoveCharacter(Position + new Vector2(-1, 0) * Time.deltaTime * 32);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            MoveCharacter(Position + new Vector2(1, 0) * Time.deltaTime * 32);
+        }
+    }
+
+    private void MoveCharacter(Vector2 NextPosition)
+    {
+        // VÉRIFIER AVEC LE WORLD SI LA TILE EST TRAVERSABLE
+        transform.position = Position = NextPosition;
+    }
+}
