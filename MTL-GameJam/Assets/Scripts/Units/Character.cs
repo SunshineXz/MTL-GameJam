b9fa5ec1884bd0542ca9ef939ff346fde559d410
Anthony Labelle-Voyez <antho.voyez@hotmail.com>
2018-01-27 01:29:32 -0500

    Base Character

---
+++ MTL-GameJam/Assets/Scripts/Units/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public int Health = 100;
+    public float MoveSpeed = 5.0f;
+    protected Vector2 Position;
+ 
+    public Character() {
+        Position = Vector2.zero;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+ 
+}
