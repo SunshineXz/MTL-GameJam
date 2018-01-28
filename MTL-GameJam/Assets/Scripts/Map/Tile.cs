15bfc812aed3a448bb3e457a13bf172848aa2870
Anthony Labelle-Voyez <antho.voyez@hotmail.com>
2018-01-27 20:59:58 -0500

    TileTypeEnum use for Wall and Water

--- MTL-GameJam/Assets/Scripts/Map/Tile.cs
+++ MTL-GameJam/Assets/Scripts/Map/Tile.cs
@@ -29,11 +29,31 @@ public class Tile : MonoBehaviour
     public bool CharacterCanGoThrough(Item characterItem)
     {
         bool CanGoThrough = false;
-        if(TileDoor)
+        
+        if(TileType == TileTypeEnum.Ground)
         {
-            if (!TileDoor.isOpen && characterItem.GetType() == typeof(Key))
+            if (TileDoor)
             {
-                TileDoor.isOpen = true;
+                if (!TileDoor.isOpen && characterItem.GetType() == typeof(Key))
+                {
+                    TileDoor.isOpen = true;
+                    CanGoThrough = true;
+                }
+                else
+                {
+                    CanGoThrough = false;
+                }
+            }
+            CanGoThrough = true;
+        }
+        else if (TileType == TileTypeEnum.Wall)
+        {
+            CanGoThrough = false;
+        }
+        else if (TileType == TileTypeEnum.Water)
+        {
+            if(characterItem.GetType() == typeof(Umbrella))
+            {
                 CanGoThrough = true;
             }
             else
@@ -41,10 +61,7 @@ public class Tile : MonoBehaviour
                 CanGoThrough = false;
             }
         }
-        else if(TileType == TileTypeEnum.Ground)
-        {
-            CanGoThrough = true;
-        }
+
         return CanGoThrough;
     }
 }
