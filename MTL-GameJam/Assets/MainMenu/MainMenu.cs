27629da5cb3af1cb08a51cfa99dd2f8debad9350
Anthony Labelle-Voyez <antho.voyez@hotmail.com>
2018-01-28 00:48:23 -0500

    MainMenu Start

---
+++ MTL-GameJam/Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public Texture backgroundTexture;
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0,0,Screen.height, Screen.width), backgroundTexture);
+    }
+}
